953be8b9f386dc9171e4ced4fd8a517521f45194
MateusHoffman <mateushoffmandev@gmail.com>
2023-03-07 13:54:05 -0300

    Create initial config API

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.use((err, req, res, next) => {
+	res.status(err.status || 500).json({ message: err.message });
+})
+
+module.exports = app;
