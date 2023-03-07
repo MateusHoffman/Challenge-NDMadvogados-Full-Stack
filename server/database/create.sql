0bbb9c203042d9e3425e3980bf283f8ac445ea07
MateusHoffman <mateushoffmandev@gmail.com>
2023-03-07 14:18:26 -0300

    Create initial queries for database

---
+++ server/database/create.sql
@@ -0,0 +1,18 @@
+DROP SCHEMA public CASCADE;
+
+CREATE SCHEMA public;
+
+CREATE TABLE time (
+  id serial NOT NULL PRIMARY KEY,
+  nome VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE jogador (
+  id serial NOT NULL PRIMARY KEY,
+  nome VARCHAR(45) NOT NULL,
+  idade int NOT NULL,
+  time_id int NOT NULL,
+  FOREIGN KEY(time_id) REFERENCES time(id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+);
---
+++ server/database/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO time (nome)
+VALUES ('Time 1'),
+        ('Time 2'),
+        ('Time 3');
+
+INSERT INTO jogador (nome, idade, time_id)
+VALUES ('jogador1', 21, 1),
+        ('jogador2', 22, 1),
+        ('jogador3', 23, 1),
+        ('jogador4', 22, 2),
+        ('jogador5', 23, 3),
+        ('jogador6', 24, 3),
+        ('jogador7', 25, 2),
+        ('jogador8', 21, 1);
