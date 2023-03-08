218256dc687e2471a73c90b583f66c288476989d
MateusHoffman <mateushoffmandev@gmail.com>
2023-03-08 19:25:32 -0300

    Update context, request get all teams with players

--- client/src/contexts/globalContext.js
+++ client/src/contexts/globalContext.js
@@ -1,5 +1,7 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+
+import api from '../service/requests';
 
 export const globalContext = createContext();
 
@@ -7,12 +9,25 @@ function GlobalProvider({ children }) {
   const [playerInfo, setPlayerInfo] = useState({});
   const [editingPlayer, setEditingPlayer] = useState(false);
 
+  const [teamsPlayers, setTeamsPlayers] = useState();
+
+  const requestApi = async () => {
+    const teamsAndPlayers = await api.get.getAllTeamsWithPlayers()
+    setTeamsPlayers(teamsAndPlayers)
+  }
+
+  useEffect(() => {
+    requestApi()
+  }, [])
+
   return (
     <globalContext.Provider
       value={ {
         playerInfo,
         setPlayerInfo,
         editingPlayer,
+        teamsPlayers,
+        setTeamsPlayers,
         setEditingPlayer,
       } }
     >
