220720fc0c25466432a8ce021391100f0f7a4be7
MateusHoffman <mateushoffmandev@gmail.com>
2023-03-08 14:39:12 -0300

    Update insert query

--- server/database/insert.sql
+++ server/database/insert.sql
@@ -1,14 +1,25 @@
 INSERT INTO time (nome)
-VALUES ('Time 1'),
-        ('Time 2'),
-        ('Time 3');
+VALUES ('CLOUD9'),
+        ('IMPERIAL'),
+        ('BIG'),
+        ('FURIA'),
+        ('FNATIC'),
+        ('MIBR'),
+        ('LIQUID');
 
 INSERT INTO jogador (nome, idade, time_id)
-VALUES ('1 Miguel', 21, 1),
-        ('2 Arthur', 22, 1),
-        ('3 Gael', 23, 1),
-        ('4 Théo', 22, 2),
-        ('5 Heitor', 23, 3),
-        ('6 Ravi', 24, 3),
-        ('7 Davi', 25, 2),
-        ('8 Bernardo', 21, 1);
+VALUES ('Gael', 23, 1),
+        ('Bernardo', 21, 1),
+        ('chelo', 21, 2),
+        ('VINI', 22, 2),
+        ('FalleN', 32, 2),
+        ('bolts', 23, 2),
+        ('JOTA', 19, 2),
+        ('Heitor', 23, 3),
+        ('Ravi', 24, 3),
+        ('João', 24, 4),
+        ('Pedro', 21, 4),
+        ('Théo', 22, 5),
+        ('Davi', 25, 5),
+        ('Juca', 25, 6),
+        ('Flavio', 25, 7);
