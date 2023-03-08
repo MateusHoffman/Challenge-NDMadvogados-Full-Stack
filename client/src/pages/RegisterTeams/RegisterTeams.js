7df5b31e3909fa8a5f19f4a16bc2042aa6ac8614
MateusHoffman <mateushoffmandev@gmail.com>
2023-03-08 13:50:11 -0300

    Create page register teams

--- client/src/pages/RegisterTeams/RegisterTeams.js
+++ client/src/pages/RegisterTeams/RegisterTeams.js
@@ -1,17 +1,41 @@
 import * as S from './styles';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
+import api from '../../service/requests';
 
 function RegisterTeams() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('')
+
+  const handleChange = ({target: {name, value}}) => {
+    if (name === 'name') setName(value)
+  }
+
+  const handleSubmit = async() => {
+    await api.post.registerTeam({name})
+    setName('')
+    navigate('/');
+  }
+
   return (
-    <div>
+    <>
       <Header />
       <S.MainContainer>
-        <h1>RegisterTeams</h1>
+        <S.TitlePage>Register Teams</S.TitlePage>
+        <S.FormRegister onSubmit={(e) => e.preventDefault()}>
+          <label>
+            Name:
+            <input type="text" name='name' value={name} onChange={(e) => handleChange(e)} />
+          </label>
+          <input type="submit" value="Submit" onClick={handleSubmit} />
+        </S.FormRegister>
       </S.MainContainer>
       <Footer />
-    </div>
+    </>
   );
 }
 
