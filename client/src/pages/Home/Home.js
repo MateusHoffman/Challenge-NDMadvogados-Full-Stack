979f065bc7ebd54d2b60263505c889890d2d81fc
MateusHoffman <mateushoffmandev@gmail.com>
2023-03-08 13:48:36 -0300

    Create page home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,16 +2,53 @@ import * as S from './styles';
 
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
+import CardTeams from '../../components/CardTeams/CardTeams';
+import api from '../../service/requests';
+import { useEffect, useState } from 'react';
 
 function Home() {
+  const [completeTeams, setCompleteTeams] = useState([])
+
+  const organizedTeams = (teams, players) => {
+    const arrOrganizedTeams = []
+    teams.forEach(t => {
+      const teamPlayers = []
+      players.forEach(p => {
+        if (p.time_id === t.id) {
+          teamPlayers.push(p.nome)
+        }
+      })
+      arrOrganizedTeams.push({team: t.nome, teamPlayers})
+    })
+    setCompleteTeams(arrOrganizedTeams)
+    return arrOrganizedTeams
+  }
+
+  const requestApi = async () => {
+    const teams = await api.get.getAllTeams()
+    const players = await api.get.getAllPlayers()
+    organizedTeams(teams, players)
+  }
+
+  useEffect(() => {
+    requestApi()
+  }, [])
+
   return (
-    <div>
+    <>
       <Header />
       <S.MainContainer>
-        <h1>HOME</h1>
+        <S.TitlePage>TEAMS</S.TitlePage>
+        <S.SectionTeams>
+          {
+            completeTeams.map((e, i) => (
+              <CardTeams key={i} info={e} />
+            ))
+          }
+        </S.SectionTeams>
       </S.MainContainer>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- client/src/pages/Home/styles.js
+++ client/src/pages/Home/styles.js
@@ -4,4 +4,22 @@ export const MainContainer = styled.main`
   margin-left: auto;
   margin-right: auto;
   max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TitlePage = styled.h1`
+  padding-top: 2rem;
+  font-size: 55px;
+  color: #73de08;
+`;
+
+export const SectionTeams = styled.section`
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  flex-wrap: wrap;
+  width: calc(100% * 0.84);
 `;
